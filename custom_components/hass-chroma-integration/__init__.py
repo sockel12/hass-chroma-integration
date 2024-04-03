500837ade8344ccdb33a2c06ccd0681770a5a5a6
Benjamin Appel <benjamin.appel1991@icloud.com>
2024-04-03 11:00:57 +0000

    test

--- custom_components/hass-chroma-integration/__init__.py
+++ custom_components/hass-chroma-integration/__init__.py
@@ -14,15 +14,26 @@ from __future__ import annotations
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.event import *
+from chromadb import HttpClient
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 # The domain of your component. Should be equal to the name of your component.
 DOMAIN = "hello_world"
 
+def a(event: Event) -> Coroutine[Any, Any, None] | None:
+    _LOGGER.debug(event.data)
+
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
+    
     # States are in the format DOMAIN.OBJECT_ID.
     hass.states.set('hello_world.Hello_World', 'Works!')
+
+    hass.bus.async_listen(EVENT_ENTITY_REGISTRY_UPDATED, a)
 
     # Return boolean to indicate that initialization was successfully.
     return True
